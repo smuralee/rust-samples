e40398c7f7987933a804ceb099ce9edb00a1e5e2
smuralee <suraj@surajmuraleedharan.com>
2020-04-13 12:47:13 +0100

    Cleanup

--- src/basics/main.rs
+++ src/basics/main.rs
@@ -2,27 +2,13 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    // Hello world!
-    println!("Hello, world!");
+    add(2, 3);
+    subtract(5, 4);
+    multiply(3, 4);
+    divide(6, 2);
 
-    // Printing the variables
-    let a = 5;
-    let b = 10;
-    println!("The values are : a = {} and b = {}", a, b);
-
-    //Shadowing - use "let" for each update of c, since the variable is immutable by default
-    let c = 10;
-    let c = c * 45;
-    let c = c / 24;
-    println!("The value of c is {}", c);
-
-    // Invoke data types examples
-    data_types();
-
-    // Initiate guess the number
     guess_the_number();
 }
-
 
 fn add(a: i32, b: i32) -> i32 {
     a + b
@@ -32,44 +18,38 @@ fn subtract(a: i32, b: i32) -> i32 {
     a - b
 }
 
+fn multiply(a: i32, b: i32) -> i32 {
+    a * b
+}
+
+fn divide(a: i32, b: i32) -> i32 {
+    a / b
+}
+
+
 #[cfg(test)]
 mod tests {
-    use super::add;
-    use super::subtract;
+    use crate::{add, divide, multiply, subtract};
 
     #[test]
     fn test_add() {
-        assert_eq!(2 + 5, add(2, 5))
+        assert_eq!(2 + 5, add(2, 5));
     }
 
     #[test]
     fn test_subtract() {
-        assert_eq!(-3, subtract(2, 5))
+        assert_eq!(-3, subtract(2, 5));
     }
-}
 
-fn data_types() {
+    #[test]
+    fn test_multiply() {
+        assert_eq!(20, multiply(10, 2));
+    }
 
-    add(2,3);
-    subtract(5,4);
-
-    let t = true;
-    let f: bool = false;
-    println!("The boolean values are : {} and {}", t, f);
-
-    let tup = (500, 6.4, 1);
-    let (x, y, z) = tup; // deconstructing a tuple
-    println!("The value of x is: {}", x);
-    println!("The value of y is: {}", y);
-    println!("The value of z is: {}", z);
-
-    let five_hundred = tup.0; //direct access of tuple using period (.)
-    let six_point_four = tup.1;
-    let one = tup.2;
-    println!("The tuple values are: ({}, {}, {})", five_hundred, six_point_four, one);
-
-    let arry = [1, 2, 3, 4, 5];
-    println!("The 4th array element is : {}", arry[3]);
+    #[test]
+    fn test_divide() {
+        assert_eq!(5, divide(25, 5));
+    }
 }
 
 fn guess_the_number() {
